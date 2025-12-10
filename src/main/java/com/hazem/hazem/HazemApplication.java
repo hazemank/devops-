9cb4701bac297655929f165f93edbb4ffc3231ef
hazem <hazem.ankoud10@gmail.com>
2025-12-10 18:29:01 +0100

    a

---
+++ src/main/java/com/hazem/hazem/HazemApplication.java
@@ -0,0 +1,13 @@
+package com.hazem.hazem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hazem/hazem/HazemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazem.hazem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
